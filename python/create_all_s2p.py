eaed7e9b4dfb4c532e905f5b167ff1ba47b8cc9b
Caleb James Smith <caleb_smith2@baylor.edu>
2021-02-11 12:51:15 -0600

    add warning comment, add files

--- python/create_all_s2p.py
+++ python/create_all_s2p.py
@@ -2,6 +2,10 @@
 
 import subprocess
 
+# WARNING:
+# Write file names ending in .vna
+# Do not include .txt (do not use .vna.txt)
+
 options = [
            #['Redo_VNA', 'TP_1p4m_35_ChD0.vna'],
            #['Redo_VNA', 'TP_1p4m_35_ChD1_redo_v1.vna'],
@@ -11,7 +15,14 @@ options = [
            #['Redo_VNA', '082820_TP_60_35cm_ChD1_KA.vna'],
            #['Redo_VNA', 'TP_35cm_60_ChCMD_redo.vna'],
             
+            # using Cu calibration plate
+            ['data', '9_feb_2021_test1_1.vna'],
+            ['data', '9_feb_2021_test1_2.vna'],
+            ['data', '9_feb_2021_test1_3.vna'],
+            ['data', '9_feb_2021_test1_4.vna'],
             ['data', '9_feb_2021_test1_5.vna'],
+            # cable 100
+            ['data', 'TP_35cm_100_test1_1.vna'],
     
 	  ]
 
