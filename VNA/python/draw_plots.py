e82900769e5904793d085cf0a3e796fba47d0e39
Sadia Khalil <skhalil@cern.ch>
2021-03-01 02:27:27 -0600

    merge the steps of creating the .s2p files and plotting the S-parameters. Its cleanup of Suyash idea.

---
+++ VNA/python/draw_plots.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import subprocess
+
+options = [
+            [1, 'input_cable_data.txt', '35', 0.2, 0.4, '0', '11'], #S11
+            [0, 'input_cable_data.txt', '35', 0.2, 0.4, '0', '12'], #S12
+            [0, 'input_cable_data.txt', '35', 0.2, 0.4, '1', '21'], #S21
+    
+	  ]
+
+command = 'python plotVNAFeatures.py --createS2p={0:<3.0f} --inputTxtFiles={1:s} --cableLength={2:s} --t1={3:<3.2f} --t2={4:<3.2f} --outputTouchstoneSubFile={5:s} --SParamterComp={6:s}'
+
+for opt in options:
+    s = command.format(opt[0], opt[1], opt[2], opt[3], opt[4], opt[5], opt[6])
+    print(s)
+    subprocess.call( [s, ""], shell=True )	
+
