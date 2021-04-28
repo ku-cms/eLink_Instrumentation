f64b14bbd031928a95c0ee6154fe02a555061070
Caleb James Smith <colorado.caleb.smith@gmail.com>
2021-04-27 19:33:47 -0500

    Fix title and labels (eye diagram data). Plot both before and after lashing data.

--- EyeDiagrams/plotComparison.py
+++ EyeDiagrams/plotComparison.py
@@ -30,44 +30,58 @@ def getData(file_name):
     return data
 
 def plotData(file_name, plot_dir):
+    verbose = False
     makeDir(plot_dir)
     data = getData(file_name)
 
     # get data from column
-    column_index = 3
-    label  = ""
-    x_vals = []
-    y_vals = []
+    column_index_1 = 3
+    column_index_2 = 6
+    data_label     = ""
+    x_vals  = []
+    y1_vals = []
+    y2_vals = []
 
     for i, row in enumerate(data):
-        # skip
+        # skip first row
         if i < 1:
             continue
+        # second row has labels
         elif i == 1:
-            label = row[column_index]
+            data_label = row[column_index_1]
+        # third row is the beginning of data values
         else:
             # WARNING: make sure to convert strings to floats!
-            x = float(i - 1)
-            y = float(row[column_index])
+            x  = float(i - 1)
+            y1 = float(row[column_index_1])
+            y2 = float(row[column_index_2])
             x_vals.append(x)
-            y_vals.append(y)
+            y1_vals.append(y1)
+            y2_vals.append(y2)
         
-        #print("{0}: {1}".format(i, row[column_index]))
+        if verbose:
+            print("{0}: {1}, {2}".format(i, row[column_index_1], row[column_index_2]))
     
     # plot
     fig, ax = plt.subplots(figsize=(6, 6))
 
-    x_array = np.array(x_vals)
-    y_array = np.array(y_vals)
-    #print(x_array)
-    #print(y_array)
+    x_array  = np.array(x_vals)
+    y1_array = np.array(y1_vals)
+    y2_array = np.array(y2_vals)
+    
+    if verbose:
+        print(x_array)
+        print(y1_array)
+        print(y2_array)
     
     ax.set_xlim([0.0, 10.0])
     ax.set_ylim([0.0, 200.0])
-    ax.set_title("Impedance of Cable 120",  fontsize=16)
-    ax.set_xlabel("Channel",                fontsize=12)
-    ax.set_ylabel("Impedance ($\Omega$)",   fontsize=12)
-    plt.scatter(x_array, y_array)
+    ax.set_title("Cable 120 Eye Diagram Data",  fontsize=16)
+    ax.set_xlabel("Channel",                    fontsize=12)
+    ax.set_ylabel(data_label,                   fontsize=12)
+    plt.scatter(x_array, y1_array, label="Before Lashing")
+    plt.scatter(x_array, y2_array, label="After Lashing")
+    plt.legend(loc='upper right', prop={'size': 12})
     plt.show()
 
 def main():
