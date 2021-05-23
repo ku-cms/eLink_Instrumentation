db5c648016eaa9600f6b82acc38e38f60c672a8a
Caleb James Smith <colorado.caleb.smith@gmail.com>
2021-05-23 10:07:34 -0500

    Loop over channels and use getFile() to find file for each channel.

--- EyeDiagrams/python/makeTables.py
+++ EyeDiagrams/python/makeTables.py
@@ -4,18 +4,34 @@ import csv
 import glob
 import tools
 
-def makeTable(input_files, output_file):
+def getFile(input_files, channel):
+    file_matches = []
+    for f in input_files:
+        if channel in f:
+            file_matches.append(f)
+    n = len(file_matches)
+    if n == 1:
+        return file_matches[0]
+    else:
+        print("ERROR: For channel {0}, there are {1} files found (should be 1).".format(channel, n))
+        print("files: {0}".format(file_matches))
+        return ""
+
+def makeTable(input_files, output_file, channels):
     verbose = False
     # row, column indices start from 0 for data matrix
     row_map    = {"label1" : 32, "label2" : 33, "value" : 34}
     column_map = {"height" : 13, "jitter" : 43, "width" : 53}
-    label_map  = {"height" : "Height (mV)", "jitter" : " Jitter (ps)", "width" : "Width (ps)"}
     unit_map   = {"height" : 10**3, "jitter" : 10**12, "width" : 10**12}
     output_column_titles = ["Index", "Module", "Channel", "Height (mV)", "Jitter (ps)", "Width (ps)"]
     with open(output_file, "w") as output_csv:
         output_writer = csv.writer(output_csv)
         output_writer.writerow(output_column_titles)
-        for i, f in enumerate(input_files):
+        for i, channel in enumerate(channels):
+            f = getFile(input_files, channel)
+            if not f:
+                print("ERROR: Unique file not found for channel {0}".format(channel))
+                return
             data = tools.getData(f)
             if verbose:
                 print(f)
@@ -26,19 +42,23 @@ def makeTable(input_files, output_file):
                     label2 = data[row_map["label2"]][column_map[key]].strip()
                     value  = float(data[row_map["value"]][column_map[key]])
                     print(" - {0}: {1} = {2:.2E}".format(label1, label2, value))
+            module = "M1"
             height = float(data[row_map["value"]][column_map["height"]]) * unit_map["height"]
             jitter = float(data[row_map["value"]][column_map["jitter"]]) * unit_map["jitter"]
             width  = float(data[row_map["value"]][column_map["width"]] ) * unit_map["width"]
-            output_row = [i+1, 0, 0, round(height, 3), round(jitter, 3), round(width, 3)]
+            # columns: index, module, channel, height, jitter, width
+            output_row = [i+1, module, channel, round(height, 3), round(jitter, 3), round(width, 3)]
             output_writer.writerow(output_row)
 
 def makeTables():
     table_dir = "tables"
     input_file_pattern = "data/Cable_158_beforeLashing/*.csv"
     output_file = "{0}/Cable_158_EyeDiagrams_beforeLashing.csv".format(table_dir)
+    channels = ["CMD", "D0", "D1", "D2", "D3"]
+
     tools.makeDir(table_dir)
     input_files = glob.glob(input_file_pattern)
-    makeTable(input_files, output_file)
+    makeTable(input_files, output_file, channels)
 
 def main():
     makeTables()
