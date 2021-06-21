e1d0aeb549e251df4b94adf3db3315692a7968e2
Caleb James Smith <colorado.caleb.smith@gmail.com>
2021-06-21 16:33:00 -0500

    Use tail to clean files and fix byte 0x96 issue.

--- EyeDiagrams/scripts/process.sh
+++ EyeDiagrams/scripts/process.sh
@@ -3,6 +3,9 @@
 # bash script to process zip file
 # zip file should contain csv files from eye diagram data
 
+# arguments
+# 1 - full path to zip file
+# 2 - cable number
 
 file=$1
 cable_num=$2 
@@ -13,14 +16,14 @@ echo "It's go time."
 echo "Input file: $file"
 
 # check that file was provided
-if [ -z $file ]
+if [ -z "$file" ]
 then
     echo "Please provide a zip file containing eye diagram statistics csv files as the first argument."
     exit 1
 fi
 
 # check that cable number was provided
-if [ -z $cable_num ]
+if [ -z "$cable_num" ]
 then
     echo "Please provide the cable number as the second argument."
     exit 1
@@ -36,10 +39,23 @@ fi
 raw_dir="Cable_"$cable_num"_raw"
 clean_dir="Cable_"$cable_num"_clean"
 
-# move files to new directory
+# unpack files in a new directory
 cd $storage_dir
 rsync -az $file .
 unzip $file
 mkdir -p $raw_dir 
 mv *.csv $raw_dir 
+
+# use tail to clean files
+# - only keep end of file
+# - fixes byte 0x96 issue: there are no 0x96 bytes in the last three lines
+mkdir -p $clean_dir
+for f in "$raw_dir"/*
+do
+    if [ -f "$f" ]
+    then
+        base=`basename "$f"`
+        tail -n 3 "$f" > "$clean_dir"/"$base"
+    fi
+done
 
