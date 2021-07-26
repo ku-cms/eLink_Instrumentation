d2a01381854dead45ff5e1ad827ecab48b873d20
Caleb James Smith <colorado.caleb.smith@gmail.com>
2021-07-26 11:52:52 -0500

    test using the same times for each cable

--- VNA/python/draw_plots.py
+++ VNA/python/draw_plots.py
@@ -16,22 +16,27 @@ options = [
             #[1, '../data/JohnCable5/', 'JohnCable5.txt', 'JohnCable5', '80',  2.0,  8.0, '0', '12'], #S12
             #[1, '../data/Cable_120_beforeLashing',  'Cable_120_beforeLashing.txt',  'Cable_120_beforeLashing',   '80',  2.0,   8.0, '0', '12'], #S12
             #[1, '../data/Cable_120_afterLashing',   'Cable_120_afterLashing.txt',   'Cable_120_afterLashing',    '80',  2.0,   8.0, '0', '12'], #S12
-            [1, '../data/Comparison_2021_07_26',   'Comparison_2021_07_26.txt',   'Comparison_2021_07_26',    '100',  2.0,   7.0, '0', '12'], #S12
+            [1, '../data/Comparison_2021_07_26', 'Comparison_2021_07_26.txt', 'Comparison_2021_07_26', '100', 0.5, 1.0, '0', '12'], #S12
 	  ]
 
 for opt in options:
     # use "times" to specify different time windows per cable
     times = []
+    
+    # v1 of times
     #times.append([0.5, 1.0])
     #times.append([2.0, 4.0])
     #times.append([2.0, 7.0])
     #times.append([2.0, 4.0])
     #times.append([2.0, 7.0])
-    times.append([0.5, 1.0])
-    times.append([0.5, 2.5])
-    times.append([0.5, 5.5])
-    times.append([0.5, 2.5])
-    times.append([0.5, 5.5])
+    
+    # v2 of times
+    #times.append([0.5, 1.0])
+    #times.append([0.5, 2.5])
+    #times.append([0.5, 5.5])
+    #times.append([0.5, 2.5])
+    #times.append([0.5, 5.5])
+    
     print("Analyzing {0}".format(opt[3]))
     #analyze(createS2p, inDir, inputTxtFiles, cableName, cableLength, t1, t2, outDir, s2pDir, subfile, comp)
     analyze(opt[0], opt[1], opt[2], opt[3], opt[4], opt[5], opt[6], "Plots", "s2pDir", opt[7], opt[8], times)
