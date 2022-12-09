072405bacc210385a1affab5a64b942ed15304dd
Caleb James Smith <colorado.caleb.smith@gmail.com>
2022-12-09 14:22:46 -0600

    Update VNA script: support 8 to 11 ns integration window, clean up input parameter type and use.

--- VNA/python/VNA_COMP.py
+++ VNA/python/VNA_COMP.py
@@ -16,13 +16,13 @@ from matplotlib import style
 import pickle as pl
 import csv
 
-
 #Transfered from other Scripts
 def name(x):
-    return x.strip(".vna").replace('Data/'+str(cable_number)+'/Plots/s2p/', "").strip("TP_")
+    return x.strip(".vna").replace('Data/'+cable_number_str+'/Plots/s2p/', "").strip("TP_")
+
 def split(word):
     return [char for char in word]
-y_plot_value=[]
+
 def display_mean_impedance(ax, t1, t2, col):##https://www.tutorialfor.com/questions-285739.htm
 
     lines = ax.get_lines()
@@ -47,7 +47,7 @@ def display_mean_impedance(ax, t1, t2, col):##https://www.tutorialfor.com/questi
 
     # get the mean value of Z for a given time difference
     Z_mean =  df.query('t >=@t1 & t<=@t2').agg({'Z': 'mean'})
-    print('Mean impedance from', t1, 'ns and', t2, 'ns =', Z_mean.values, 'for', lines[0])
+    print('Mean impedance from', t1, 'ns to', t2, 'ns =', Z_mean.values, 'for', lines[0])
     # plot the average line
     x_coor = [t1, t2]
     y_coor = [Z_mean, Z_mean]
@@ -78,15 +78,20 @@ parser= OptionParser()
 #    print("Please change your file name")
 #    exit()
 
-cable_number = str(input("Enter cable number: "))
+cable_number = int(input("Enter cable number: "))
 cable_type   = int(input("Enter cable type (1, 2, 3, 4): "))
-cable_length = input("Enter cable length in cm (35, 80, 100, 140, 160, 200): ")
-int_window = input('integration window 2-5ns(0) or variable(1)? (0/1):')
+cable_length = int(input("Enter cable length in cm [0, 35, 80, 100, 140, 160, 180, 200]: "))
+int_window   = int(input('integration window 2-5ns (0), 8-11ns (1), or variable (2)? [0,1,2]:'))
+
+cable_number_str = str(cable_number)
+cable_type_str   = str(cable_type)
+cable_length_str = str(cable_length)
 
 Pre_list = []
+y_plot_value=[]
 
-print("\n Detecting Files...")
-for root, dirs, files in os.walk("./Data/"+cable_number, topdown=False):
+print("Loading files")
+for root, dirs, files in os.walk("./Data/"+cable_number_str, topdown=False):
    for name in files:
       if ".txt" in os.path.join(name):
         Pre_list.append(os.path.join(name))
@@ -101,11 +106,11 @@ filename_list = []
 color_list = ['b', 'r', 'g', 'w', 'm']
 
 for i in range(n_channels):
-    ff_list.append(f"Data/"+cable_number+f"/{Pre_list[i]}")
+    ff_list.append(f"Data/"+cable_number_str+f"/{Pre_list[i]}")
     filename_list.append(f"{Pre_list[i]}")
 
-makeDir("Data/"+cable_number+"/Plots")
-makeDir("Data/"+cable_number+"/Plots/s2p")
+makeDir("Data/"+cable_number_str+"/Plots")
+makeDir("Data/"+cable_number_str+"/Plots/s2p")
 
 IDchecker = 0
 Breaker = True
@@ -118,21 +123,21 @@ while Breaker == True:
     print("Creating s2p files for ", FILE)
 
     parser.add_option('--basename', metavar='T', type='string', action='store',
-                      default='Data/'+str(cable_number)+'/'+FILE, #31, 15, 33 #calibration_test.vna
+                      default='Data/'+cable_number_str+'/'+FILE, #31, 15, 33 #calibration_test.vna
                       dest='basename',
                       help='input text file')
 
     parser.add_option('--directory', metavar='T', type='string', action='store',
-                      default='Data/'+str(cable_number)+'/'+'Plots/',
+                      default='Data/'+cable_number_str+'/'+'Plots/',
                       dest='directory',
                       help='directory to store plots')
 
     (options,args) = parser.parse_args()
     # ==========end: options =============
     basename1 = options.basename
-    basename2 = basename1.replace('Data/'+str(cable_number)+'/', 'Data/'+str(cable_number)+'/'+'Plots/s2p/')
+    basename2 = basename1.replace('Data/'+cable_number_str+'/', 'Data/'+cable_number_str+'/'+'Plots/s2p/')
     dir_in= options.directory
-    cable = basename1.replace('Data/'+str(cable_number)+'/', "")
+    cable = basename1.replace('Data/'+cable_number_str+'/', "")
 
     infile = pd.read_csv(basename1, names=['pt','f','s11R','s11I','s12R','s12I','s13R','s13I','s14R','s14I'], delim_whitespace=True, skiprows=1)
     infile.dropna(how='all')
@@ -259,37 +264,45 @@ while Breaker == True:
 print("Plots can be now found in the Plots folder of the Cable\n")
 
 # specify time integration window as a function of length
-if int_window == "1":
-    if cable_length == "35":
-        t1 = 2.00
-        t2 = 4.00
-    elif cable_length == "80":
-        t1 = 2.00
-        t2 = 6.00
-    elif cable_length == "100":
-        t1 = 2.00
-        t2 = 7.00
-    elif cable_length == "140":
-        t1 = 2.00
-        t2 = 9.00
-    elif cable_length == "160":
-        t1 = 2.00
-        t2 = 10.00
-    elif cable_length == "180":
-        t1 = 2.00
-        t2 = 11.00
-    elif cable_length == "200":
-        t1 = 2.00
-        t2 = 12.00
-    elif cable_length == "0":
+if int_window == 0:
+    t1 = 2.00
+    t2 = 5.00
+elif int_window == 1:
+    t1 = 8.00
+    t2 = 11.00
+elif int_window == 2:
+    if cable_length == 0:
         t1 = 0.00
         t2 = 1.50
+    elif cable_length == 35:
+        t1 = 2.00
+        t2 = 4.00
+    elif cable_length == 80:
+        t1 = 2.00
+        t2 = 6.00
+    elif cable_length == 100:
+        t1 = 2.00
+        t2 = 7.00
+    elif cable_length == 140:
+        t1 = 2.00
+        t2 = 9.00
+    elif cable_length == 160:
+        t1 = 2.00
+        t2 = 10.00
+    elif cable_length == 180:
+        t1 = 2.00
+        t2 = 11.00
+    elif cable_length == 200:
+        t1 = 2.00
+        t2 = 12.00
     else:
-        t1=0
-        t2=0
-elif int_window == "0":
-    t1 = 2
-    t2 = 5
+        print("ERROR: the cable length '{0}' is not valid; it must be one of these: [0, 35, 80, 100, 140, 160, 180, 200] cm.".format(cable_length))
+        t1 = 0.00
+        t2 = 0.00
+else:
+    print("ERROR: the integration window '{0}' is not valid; it must be one of these: [0,1,2].".format(int_window))
+    t1 = 0.00
+    t2 = 0.00
 
 comps = ['11','12', '21']
 subfiles = ['0','0','1',]
@@ -317,7 +330,7 @@ while Breaker == True:
 
     net_list = []
     for channel in range(n_channels):
-        net_list.append(rf.Network("Data/"+cable_number+"/Plots/s2p/"+filename_list[channel].replace(".txt","")+'_'+subfile+'.s2p', f_unit='ghz'))
+        net_list.append(rf.Network("Data/"+cable_number_str+"/Plots/s2p/"+filename_list[channel].replace(".txt","")+'_'+subfile+'.s2p', f_unit='ghz'))
 
     #netref = rf.network.Network(out_dir+'/'+sub_out_dir+'/straight_SMA.vna_'+subfile+'.s2p', f_unit='ghz')
 
@@ -344,15 +357,15 @@ while Breaker == True:
 
         with open("Impedence_List.csv", "a") as Ana:
             Ana.write("Cable_number,Length,Type, Time Interval, S11, S12, S21, Comments")
-            Ana.write(str(cable_number)+","+str(cable_length)+","+str(cable_type)+","+str(t1)+ "-"+str(t2)+","+str(Comment)+",")
+            Ana.write(cable_number_str+","+cable_length_str+","+cable_type_str+","+str(t1)+ "-"+str(t2)+","+str(Comment)+",")
             for i in y_plot_value:
                 Ana.write(str(i)+",")
             Ana.write("\n")
             Ana.close()
 
-        with open("Data/"+cable_number+"/Impedence_List.csv", "a") as Ana2:
+        with open("Data/"+cable_number_str+"/Impedence_List.csv", "a") as Ana2:
             Ana2.write("Cable_number,Length,Type, Time Interval, S11, S12, , Comments")
-            Ana2.write(str(cable_number)+","+str(cable_length)+","+str(cable_type)+","+str(t1)+ "-"+str(t2)+","+str(Comment)+",")
+            Ana2.write(cable_number_str+","+cable_length_str+","+cable_type_str+","+str(t1)+ "-"+str(t2)+","+str(Comment)+",")
             for i in y_plot_value:
                 Ana2.write(str(i)+",")
             Ana2.write("\n")
@@ -363,7 +376,7 @@ while Breaker == True:
         set_axes(ax1, 'Time Domain', 0.0, 200.0, 0.0, 30.0, 0)
         set_axes(ax0, 'Time Domain', -75.0, 75.0, 0.0, 6.0, 0)
 
-        fig0.savefig("Data/"+cable_number+"/Plots/"+cable_number+'cm_freq_time_Z_rf_'+"S"+comp+'.png')
+        fig0.savefig("Data/"+cable_number_str+"/Plots/"+cable_number_str+'cm_freq_time_Z_rf_'+"S"+comp+'.png')
 
         IDchecker+=1
 
@@ -371,4 +384,3 @@ while Breaker == True:
             break
 
 print("Data analysed. Plots stored in Plots folder.")
-
