238aa736a4d4dc565caf5a02a498220027480fde
Caleb James Smith <colorado.caleb.smith@gmail.com>
2023-03-06 14:31:09 -0700

    Make function that gets average values more general.

--- Analysis/python/analyze.py
+++ Analysis/python/analyze.py
@@ -24,16 +24,16 @@ def getColumnNum(name):
         print("ERROR in getColumnNum(): the name '{0}' is not a valid column!".format(name))
     return result
 
-# check that all areas are floats
-def validAreas(areas):
-    for area in areas:
-        if not tools.is_float(area):
+# check that all values are floats
+def validValues(values):
+    for value in values:
+        if not tools.is_float(value):
             return False
     return True
 
-# convert areas to floats
-def convertAreas(areas):
-    result = [float(area) for area in areas]
+# convert values to floats
+def convertToFloats(values):
+    result = [float(value) for value in values]
     return result
 
 # get cable lengths
@@ -53,35 +53,40 @@ def getLengths(data):
     return result
 
 # get cable eye bert areas
-def getAreas(data):
+def getMeanValues(data, column_names):
     result = {}
 
     cable_number_index      = getColumnNum("cable_number")
-    eye_bert_area_CMD_index = getColumnNum("eye_bert_area_CMD")
-    eye_bert_area_D0_index  = getColumnNum("eye_bert_area_D0")
-    eye_bert_area_D1_index  = getColumnNum("eye_bert_area_D1")
-    eye_bert_area_D2_index  = getColumnNum("eye_bert_area_D2")
-    eye_bert_area_D3_index  = getColumnNum("eye_bert_area_D3")
+    #eye_bert_area_CMD_index = getColumnNum("eye_bert_area_CMD")
+    #eye_bert_area_D0_index  = getColumnNum("eye_bert_area_D0")
+    #eye_bert_area_D1_index  = getColumnNum("eye_bert_area_D1")
+    #eye_bert_area_D2_index  = getColumnNum("eye_bert_area_D2")
+    #eye_bert_area_D3_index  = getColumnNum("eye_bert_area_D3")
+
+    value_indices = [getColumnNum(name) for name in column_names]
     
     for row in data:
         cable_number = row[cable_number_index]
-        raw_areas = []
-        raw_areas.append(row[eye_bert_area_CMD_index])
-        raw_areas.append(row[eye_bert_area_D0_index])
-        raw_areas.append(row[eye_bert_area_D1_index])
-        raw_areas.append(row[eye_bert_area_D2_index])
-        raw_areas.append(row[eye_bert_area_D3_index])
-        # check that cable number and areas are valid
-        valid_cable = tools.is_int(cable_number)
-        valid_areas = validAreas(raw_areas)
-        if valid_cable and valid_areas:
+        #raw_areas = []
+        #raw_areas.append(row[eye_bert_area_CMD_index])
+        #raw_areas.append(row[eye_bert_area_D0_index])
+        #raw_areas.append(row[eye_bert_area_D1_index])
+        #raw_areas.append(row[eye_bert_area_D2_index])
+        #raw_areas.append(row[eye_bert_area_D3_index])
+        
+        raw_values = [row[i] for i in value_indices]
+        
+        # check that cable number and values are valid
+        valid_cable     = tools.is_int(cable_number)
+        valid_values    = validValues(raw_values)
+        if valid_cable and valid_values:
             cable_number = int(cable_number)
-            areas   = convertAreas(raw_areas)
-            mean    = np.mean(areas) 
-            std     = np.std(areas) 
+            values  = convertToFloats(raw_values)
+            mean    = np.mean(values) 
+            std     = np.std(values) 
             result[cable_number] = {}
-            result[cable_number]["eye_bert_area_mean"]    = mean
-            result[cable_number]["eye_bert_area_std"]     = std
+            result[cable_number]["mean"]    = mean
+            result[cable_number]["std"]     = std
 
     return result
 
@@ -90,18 +95,18 @@ def plot_area_vs_length(lengths, eye_bert_areas, plot_dir):
     y_vals = []
     y_errs = []
     for cable_number in eye_bert_areas:
-        length = lengths[cable_number]
-        eye_bert_area_mean  = eye_bert_areas[cable_number]["eye_bert_area_mean"]
-        eye_bert_area_std   = eye_bert_areas[cable_number]["eye_bert_area_std"]
-        rel_err = eye_bert_area_std / eye_bert_area_mean
-        print("cable number: {0}, length: {1} m, eye_bert_area_mean: {2:.1f}, eye_bert_area_std: {3:.1f}, rel_err: {4:.3f}".format(cable_number, length, eye_bert_area_mean, eye_bert_area_std, rel_err))
+        length  = lengths[cable_number]
+        mean    = eye_bert_areas[cable_number]["mean"]
+        std     = eye_bert_areas[cable_number]["std"]
+        rel_err = std / mean
+        print("cable number: {0}, length: {1} m, mean: {2:.1f}, std: {3:.1f}, rel_err: {4:.3f}".format(cable_number, length, mean, std, rel_err))
         # do not include e-link if relative error is too large
         if rel_err > 0.10:
             print("WARNING: not including e-link {0}, length {1} m due to a large relative error: {2:.3f}".format(cable_number, length, rel_err))
         else:
             x_vals.append(length)
-            y_vals.append(eye_bert_area_mean)
-            y_errs.append(eye_bert_area_std)
+            y_vals.append(mean)
+            y_errs.append(std)
 
     output_file = "{0}/eye_bert_area_vs_length.pdf".format(plot_dir)
     title   = "Eye BERT Areas"
@@ -117,7 +122,8 @@ def analyze(input_file, plot_dir):
     tools.makeDir(plot_dir)
     
     lengths = getLengths(data)
-    eye_bert_areas = getAreas(data)
+    column_names = ["eye_bert_area_CMD", "eye_bert_area_D0", "eye_bert_area_D1", "eye_bert_area_D2", "eye_bert_area_D3"]
+    eye_bert_areas = getMeanValues(data, column_names)
 
     plot_area_vs_length(lengths, eye_bert_areas, plot_dir)
 
