a24f61617bc780d0fd7002b88ee9a9fed4be6f42
Caleb James Smith <colorado.caleb.smith@gmail.com>
2024-10-10 09:08:25 -0500

    Get e-link branches based on wiring type.

--- scripts/calc_length.py
+++ scripts/calc_length.py
@@ -1,8 +1,27 @@
 # calc_length.py
 
+# TODO:
+# - Require valid user inputs
+# - Convert user inputs to int when applicable
+# - Create class
+
+# DONE:
+
+# cable branches based on cable type
+cable_branches = {
+    "3p2" : ["A", "B", "C"],
+    "2p2" : ["A", "C"],
+    "2p3" : ["A", "B"],
+    "1p3" : ["A"]
+}
+
+# supported cable types
+cable_types = list(cable_branches.keys())
+
 # Calculate length
 def CalculateLength(wiring_type, length_type, n_elinks, loss):
-    pass
+    branches = cable_branches[wiring_type]
+    print(f"branches: {branches}")
 
 def run():
     print("Calculating twisted pair wire lengths.")
@@ -13,6 +32,10 @@ def run():
     length_type = input("Enter e-link length type: ")
     n_elinks    = input("Enter number of e-links: ")
     loss        = input("Enter loss per twisted pair (mm): ")
+
+    # Idea:
+    # support both formats for type: 3.2 and 3p2
+    # convert on format to the other using replace()
     
     print("--------------------------------------")
     print(f"wiring_type: {wiring_type}")
