9feb5ec5cbcceb847dd165997b76c067d56e9d87
Caleb James Smith <colorado.caleb.smith@gmail.com>
2024-10-13 07:44:17 -0400

    Require valid wiring type.

--- scripts/calc_length.py
+++ scripts/calc_length.py
@@ -7,7 +7,9 @@
 
 # DONE:
 
-# cable branches based on cable type
+import tools
+
+# cable branches based on wiring type
 cable_branches = {
     "3p2" : ["A", "B", "C"],
     "2p2" : ["A", "C"],
@@ -15,12 +17,19 @@ cable_branches = {
     "1p3" : ["A"]
 }
 
-# supported cable types
-cable_types = list(cable_branches.keys())
+# supported wiring types
+wiring_types = list(cable_branches.keys())
 
 # Calculate length
 def CalculateLength(wiring_type, length_type, n_elinks, loss):
-    branches = cable_branches[wiring_type]
+    branches = []
+    wiring_type_is_valid = tools.is_valid_cable_type(wiring_types, wiring_type)
+    if wiring_type_is_valid:
+        branches = cable_branches[wiring_type]
+    else:
+        print(f"ERROR: {wiring_type} is not a valid wiring type.")
+        print(f"Please enter a valid wiring type: {wiring_types}")
+        return
     print(f"branches: {branches}")
 
 def run():
@@ -43,7 +52,9 @@ def run():
     print(f"n_elinks: {n_elinks}")
     print(f"loss: {loss} mm")
 
-    # Calculate length
+    # Calculate length;
+    # may split into two steps: 1 e-link vs. n e-links
+    # so that we can print out both...
     CalculateLength(wiring_type, length_type, n_elinks, loss)
 
 def main():
