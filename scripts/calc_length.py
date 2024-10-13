0105b5cdd74eed6fa01aabd6980f4302edc64c3f
Caleb James Smith <colorado.caleb.smith@gmail.com>
2024-10-13 10:31:28 -0400

    Return if length is negative. Change print format. Rename functions and variables.

--- scripts/calc_length.py
+++ scripts/calc_length.py
@@ -20,8 +20,9 @@ cable_branches = {
 # supported wiring types
 wiring_types = list(cable_branches.keys())
 
-# Calculate length
-def CalculateLength(wiring_type, length_type, n_elinks, loss):
+# calculate wire length for one e-link
+def CalcWireLengthElink(wiring_type, length_type, loss):
+    wire_length_elink = -1
     branches = []
     wiring_type_is_valid = tools.is_valid_cable_type(wiring_types, wiring_type)
     if wiring_type_is_valid:
@@ -29,33 +30,54 @@ def CalculateLength(wiring_type, length_type, n_elinks, loss):
     else:
         print(f"ERROR: {wiring_type} is not a valid wiring type.")
         print(f"Please enter a valid wiring type: {wiring_types}")
-        return
+        return wire_length_elink
     print(f"branches: {branches}")
+    return wire_length_elink
+
+# calculate wire length for a batch of n e-links
+def CalcWireLengthBatch(n_elinks, wire_length_elink):
+    wire_length_batch = n_elinks * wire_length_elink
+    return wire_length_batch
 
 def run():
-    print("Calculating twisted pair wire lengths.")
-    print("--------------------------------------")
+    print("Calculating twisted pair wire length.")
+    print("-------------------------------------")
     
     # User input
     wiring_type = input("Enter e-link wiring type: ")
     length_type = input("Enter e-link length type: ")
-    n_elinks    = input("Enter number of e-links: ")
     loss        = input("Enter loss per twisted pair (mm): ")
+    n_elinks    = input("Enter number of e-links: ")
+
+    # FIXME: Convert user inputs to int when applicable
 
     # Idea:
     # support both formats for type: 3.2 and 3p2
     # convert on format to the other using replace()
     
-    print("--------------------------------------")
-    print(f"wiring_type: {wiring_type}")
-    print(f"length_type: {length_type}")
-    print(f"n_elinks: {n_elinks}")
-    print(f"loss: {loss} mm")
+    print("-------------------------------------")
+    print(f" - wiring_type: {wiring_type}")
+    print(f" - length_type: {length_type}")
+    print(f" - loss: {loss} mm")
+    print(f" - n_elinks: {n_elinks}")
+    print("-------------------------------------")
 
     # Calculate length;
     # may split into two steps: 1 e-link vs. n e-links
     # so that we can print out both...
-    CalculateLength(wiring_type, length_type, n_elinks, loss)
+
+    # calculate wire length for one e-link
+    wire_length_elink = CalcWireLengthElink(wiring_type, length_type, loss)
+
+    # if length < 0 (invalid), return
+    if wire_length_elink < 0:
+        return
+
+    # calculate wire length for a batch of n e-links
+    wire_length_batch = CalcWireLengthBatch(n_elinks, wire_length_elink)
+
+    print(f"wire_length_elink: {wire_length_elink}")
+    print(f"wire_length_batch: {wire_length_batch}")
 
 def main():
     run()
