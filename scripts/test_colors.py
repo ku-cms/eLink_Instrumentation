111900a7b5f798d98109fc21b665420d2b019692
Caleb James Smith <colorado.caleb.smith@gmail.com>
2024-10-23 15:36:21 -0500

    For test colors script, customize fix color command based on OS.

--- scripts/test_colors.py
+++ scripts/test_colors.py
@@ -1,25 +1,37 @@
 # test_colors.py
-
+#
 # Colorama:
 # https://pypi.org/project/colorama/
-
+#
 # Warning:
 #
 # By default, colors will not work in Windows Command Prompt;
 # you will only get ANSI escape codes.
 #
-# To fix this, you need to use this line:
+# For Windows, use this line:
 # init(convert=True)
+#
+# For other operating systems (platforms), use this line:
+# init(convert=False)
 #
 # Fix colors for Windows Command Prompt:
 # https://stackoverflow.com/questions/9848889/colorama-for-python-not-returning-colored-print-lines-on-windows
+#
 
 from colorama import Fore, Back, Style, init
+import sys
 
-# Fixes colors for Windows Command Prompt:
-init(convert=True)
+# Check operating system (platform)
+if sys.platform == "win32":
+    # For Windows Command Prompt, fix colors using convert=True:
+    init(convert=True)
+else:
+    # For other OS/platforms, use convert=False:
+    init(convert=False)
 
+# test colors
 def test_colors():
+    print(f"Operating System (platform): {sys.platform}")
     print("Testing colors...")
     print("1 - " + Fore.BLACK    + "BLACK"    + Fore.RESET)
     print("2 - " + Fore.RED      + "RED"      + Fore.RESET)
