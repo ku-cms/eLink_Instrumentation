7c854246f3c5d545cc523e38f10ab520e270af13
Caleb James Smith <colorado.caleb.smith@gmail.com>
2024-10-24 09:48:34 -0500

    Add bash scripts: test colors and wire length.

---
+++ scripts/test_colors.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+# test_colors.sh
+
+python3 ~/eLink_Instrumentation/scripts/test_colors.py
+
---
+++ scripts/wire_length.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+# wire_length.sh
+
+python3 ~/eLink_Instrumentation/scripts/wire_length.py
+
