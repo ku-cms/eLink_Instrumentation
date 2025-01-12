59cc3f71f7324b838479aa233af3ed598c8b3e7b
Caleb James Smith <colorado.caleb.smith@gmail.com>
2025-01-12 15:02:27 -0600

    New script to backup and collect results (runs both scripts).

---
+++ scripts/backupAndCollectResults.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+#
+# backupAndCollectResults.sh
+#
+# Developed by the KU CMS group.
+#
+# -------------------------- #
+# Author:   Caleb Smith
+# Date:     January 12, 2025
+# -------------------------- #
+
+# Backup results
+./backupResults.sh
+
+# Collect results
+./collectResults.sh
+
