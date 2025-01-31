2a27c120d3cf4892ff5eab752cbae31dcc650c08
Caleb James Smith <colorado.caleb.smith@gmail.com>
2025-01-31 17:20:38 -0600

    Backup two sets of results: data and plots.

--- scripts/backupResults.sh
+++ scripts/backupResults.sh
@@ -9,10 +9,17 @@
 # Date:     January 12, 2025
 # -------------------------- #
 
-VPN_URL=kuanywhere.ku.edu
-SERVER_PATH=smb://resfs.home.ku.edu/GROUPS/PHSX/General/BEAN_GRP
-RESULTS_DIR=/Volumes/BEAN_GRP/EyeBERTAutomation/automation_results
+# Results paths (remote)
+RESULTS_DIR=/Volumes/BEAN_GRP/EyeBERTAutomation
+RESULTS_PLOTS_DIR=$RESULTS_DIR/automation_results
+
+# Backup paths (local)
 BACKUP_DIR=~/CMS/Tracker/e-links/EyeBERTAutomation
+BACKUP_DATA_DIR=$BACKUP_DIR/automation_data
+
+# Network paths
+SERVER_PATH=smb://resfs.home.ku.edu/GROUPS/PHSX/General/BEAN_GRP
+VPN_URL=kuanywhere.ku.edu
 
 # Confirm access to the results stored on the R drive.
 if [ ! -d "$RESULTS_DIR" ];
@@ -24,13 +31,23 @@ then
     exit 1
 fi
 
-# Backup results.
-echo "Backing up results..."
-echo " - source (R drive): ${RESULTS_DIR}"
+# Create backup directories
+mkdir -p $BACKUP_DIR
+mkdir -p $BACKUP_DATA_DIR
+
+# Backup data
+echo "Backing up data..."
+echo " - source (R drive): ${RESULTS_DIR}/*.xlsx"
+echo " - destination (local): ${BACKUP_DATA_DIR}"
+
+rsync -az $RESULTS_DIR/*.xlsx $BACKUP_DATA_DIR
+
+# Backup plots
+echo "Backing up plots..."
+echo " - source (R drive): ${RESULTS_PLOTS_DIR}"
 echo " - destination (local): ${BACKUP_DIR}"
 
-mkdir -p $BACKUP_DIR
-rsync -az $RESULTS_DIR $BACKUP_DIR
+rsync -az $RESULTS_PLOTS_DIR $BACKUP_DIR
 
 echo "Finished backing up results!"
 
