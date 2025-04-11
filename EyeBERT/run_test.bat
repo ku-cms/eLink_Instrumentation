1ffb11e67dd7cff90be8ca47af2f3d785cc4ca65
Caleb James Smith <colorado.caleb.smith@gmail.com>
2025-04-11 18:01:11 -0500

    Create new set of files to use with the new KC705 tested on April 11, 2025.

--- EyeBERT/run_test.bat
+++ EyeBERT/run_test.bat
@@ -1,5 +1,6 @@
 cd C:\Users\Public\Documents\cable_tests
 
-d:\vivado\2020.2\bin\vivado.bat -mode batch -source run_test.tcl
+:: d:\vivado\2020.2\bin\vivado.bat -mode batch -source run_test.tcl
+d:\vivado\2020.2\bin\vivado.bat -mode batch -source run_test_KC705_v2.tcl
 
 exit
