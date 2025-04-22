99b860b2fb40f8dad3f5eff03bd6054f4397b22b
Caleb James Smith <colorado.caleb.smith@gmail.com>
2025-04-21 19:19:49 -0500

    Update tcl files to use with the KC705 from Rice setup on April 18, 2025.

--- EyeBERT/run_test_KC705_v2.tcl
+++ EyeBERT/run_test_KC705_v2.tcl
@@ -24,19 +24,19 @@ refresh_hw_device [lindex [get_hw_devices xc7k325t_0] 0]
 
 # setup links
 set xil_newLinks [list]
-set xil_newLink [create_hw_sio_link -description {Link 0} [lindex [get_hw_sio_txs localhost:3121/xilinx_tcf/Digilent/210203A3CFC8A/0_1_0/IBERT/Quad_117/MGT_X0Y8/TX] 0] [lindex [get_hw_sio_rxs localhost:3121/xilinx_tcf/Digilent/210203A3CFC8A/0_1_0/IBERT/Quad_117/MGT_X0Y8/RX] 0] ]
+set xil_newLink [create_hw_sio_link -description {Link 0} [lindex [get_hw_sio_txs localhost:3121/xilinx_tcf/Digilent/210203A3CE0EA/0_1_0/IBERT/Quad_117/MGT_X0Y8/TX] 0] [lindex [get_hw_sio_rxs localhost:3121/xilinx_tcf/Digilent/210203A3CE0EA/0_1_0/IBERT/Quad_117/MGT_X0Y8/RX] 0] ]
 lappend xil_newLinks $xil_newLink
 set xil_newLinkGroup [create_hw_sio_linkgroup -description {Link Group 0} [get_hw_sio_links $xil_newLinks]]
 unset xil_newLinks
 
 # assign link settings
-set_property TXPRE {0.00 dB (00000)} [get_hw_sio_links {localhost:3121/xilinx_tcf/Digilent/210203A3CFC8A/0_1_0/IBERT/Quad_117/MGT_X0Y8/TX->localhost:3121/xilinx_tcf/Digilent/210203A3CFC8A/0_1_0/IBERT/Quad_117/MGT_X0Y8/RX}]
-set_property TXPOST {0.00 dB (00000)} [get_hw_sio_links {localhost:3121/xilinx_tcf/Digilent/210203A3CFC8A/0_1_0/IBERT/Quad_117/MGT_X0Y8/TX->localhost:3121/xilinx_tcf/Digilent/210203A3CFC8A/0_1_0/IBERT/Quad_117/MGT_X0Y8/RX}]
+set_property TXPRE {0.00 dB (00000)} [get_hw_sio_links {localhost:3121/xilinx_tcf/Digilent/210203A3CE0EA/0_1_0/IBERT/Quad_117/MGT_X0Y8/TX->localhost:3121/xilinx_tcf/Digilent/210203A3CE0EA/0_1_0/IBERT/Quad_117/MGT_X0Y8/RX}]
+set_property TXPOST {0.00 dB (00000)} [get_hw_sio_links {localhost:3121/xilinx_tcf/Digilent/210203A3CE0EA/0_1_0/IBERT/Quad_117/MGT_X0Y8/TX->localhost:3121/xilinx_tcf/Digilent/210203A3CE0EA/0_1_0/IBERT/Quad_117/MGT_X0Y8/RX}]
 
-set_property TXDIFFSWING {741 mV (0111)} [get_hw_sio_links {localhost:3121/xilinx_tcf/Digilent/210203A3CFC8A/0_1_0/IBERT/Quad_117/MGT_X0Y8/TX->localhost:3121/xilinx_tcf/Digilent/210203A3CFC8A/0_1_0/IBERT/Quad_117/MGT_X0Y8/RX}]
+set_property TXDIFFSWING {741 mV (0111)} [get_hw_sio_links {localhost:3121/xilinx_tcf/Digilent/210203A3CE0EA/0_1_0/IBERT/Quad_117/MGT_X0Y8/TX->localhost:3121/xilinx_tcf/Digilent/210203A3CE0EA/0_1_0/IBERT/Quad_117/MGT_X0Y8/RX}]
 
-set_property TX_PATTERN {PRBS 7-bit} [get_hw_sio_links {localhost:3121/xilinx_tcf/Digilent/210203A3CFC8A/0_1_0/IBERT/Quad_117/MGT_X0Y8/TX->localhost:3121/xilinx_tcf/Digilent/210203A3CFC8A/0_1_0/IBERT/Quad_117/MGT_X0Y8/RX}]
-set_property RX_PATTERN {PRBS 7-bit} [get_hw_sio_links {localhost:3121/xilinx_tcf/Digilent/210203A3CFC8A/0_1_0/IBERT/Quad_117/MGT_X0Y8/TX->localhost:3121/xilinx_tcf/Digilent/210203A3CFC8A/0_1_0/IBERT/Quad_117/MGT_X0Y8/RX}]
+set_property TX_PATTERN {PRBS 7-bit} [get_hw_sio_links {localhost:3121/xilinx_tcf/Digilent/210203A3CE0EA/0_1_0/IBERT/Quad_117/MGT_X0Y8/TX->localhost:3121/xilinx_tcf/Digilent/210203A3CE0EA/0_1_0/IBERT/Quad_117/MGT_X0Y8/RX}]
+set_property RX_PATTERN {PRBS 7-bit} [get_hw_sio_links {localhost:3121/xilinx_tcf/Digilent/210203A3CE0EA/0_1_0/IBERT/Quad_117/MGT_X0Y8/TX->localhost:3121/xilinx_tcf/Digilent/210203A3CE0EA/0_1_0/IBERT/Quad_117/MGT_X0Y8/RX}]
 
 # the commit_hw_sio command is only required once at the end to apply the link settings
-commit_hw_sio -non_blocking [get_hw_sio_links {localhost:3121/xilinx_tcf/Digilent/210203A3CFC8A/0_1_0/IBERT/Quad_117/MGT_X0Y8/TX->localhost:3121/xilinx_tcf/Digilent/210203A3CFC8A/0_1_0/IBERT/Quad_117/MGT_X0Y8/RX}]
+commit_hw_sio -non_blocking [get_hw_sio_links {localhost:3121/xilinx_tcf/Digilent/210203A3CE0EA/0_1_0/IBERT/Quad_117/MGT_X0Y8/TX->localhost:3121/xilinx_tcf/Digilent/210203A3CE0EA/0_1_0/IBERT/Quad_117/MGT_X0Y8/RX}]
