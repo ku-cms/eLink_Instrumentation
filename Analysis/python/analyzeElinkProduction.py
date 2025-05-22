c7d735a191c55e79ed12d3bdd6684357c029634d
Caleb James Smith <colorado.caleb.smith@gmail.com>
2025-05-21 22:55:34 -0500

    Make separate functions for sample data and e-link production data.

--- Analysis/python/analyzeElinkProduction.py
+++ Analysis/python/analyzeElinkProduction.py
@@ -10,13 +10,11 @@ def createSampleData():
     production = np.random.randint(0, 20, size=len(dates))
     return (dates, production)
 
-def analyzeElinkProduction(input_file, plot_dir):
-    print("Analyzing e-link production...")
-    print(f" - input file: {input_file}")
+def analyzeSampleData(plot_dir):
+    print("Analyzing sample data...")
     print(f" - plot directory: {plot_dir}")
     
     tools.makeDir(plot_dir)
-
     dates, production = createSampleData()
     cumulative_production = np.cumsum(production)
 
@@ -30,10 +28,32 @@ def analyzeElinkProduction(input_file, plot_dir):
     
     print("Done!")
 
+def analyzeElinkProductionData(input_file, plot_dir):
+    print("Analyzing e-link production data...")
+    print(f" - input file: {input_file}")
+    print(f" - plot directory: {plot_dir}")
+    
+    tools.makeDir(plot_dir)
+    dates, production = createSampleData()
+    cumulative_production = np.cumsum(production)
+
+    plot_name = "elink_production"
+    title = "Cumulative e-link production"
+    x_label = "Time (week)"
+    y_label = "Number of e-links"
+    x_lim = []
+    y_lim = []
+    plot.makeCumulativePlot(dates, cumulative_production, plot_dir, plot_name, title, x_label, y_label, x_lim, y_lim)
+    
+    print("Done!")
+
 def main():
+    plot_dir    = "sample_data_plots"
+    analyzeSampleData(plot_dir)
+    
     input_file  = ""
     plot_dir    = "elink_production_plots"
-    analyzeElinkProduction(input_file, plot_dir)
+    analyzeElinkProductionData(input_file, plot_dir)
 
 if __name__ == "__main__":
     main()
