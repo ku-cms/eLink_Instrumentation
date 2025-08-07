94fe187a0f11f574e07375169efe5a0f24d9260a
Caleb James Smith <colorado.caleb.smith@gmail.com>
2025-08-07 18:36:52 -0500

    Remove old commented code.

--- Analysis/python/prepareElinkProductionData.py
+++ Analysis/python/prepareElinkProductionData.py
@@ -30,36 +30,36 @@ def getTodayDate():
 
 def addDateToFileName(original_file_name):
     print("Adding date to file name...")
+    
     today_date = getTodayDate()
     today_date = today_date.replace("-", "_")
-    # split_name = original_file_name.split(".")
-    # new_file_name = f"{split_name[0]}_{today_date}.{split_name[1]}"
     file_base, file_extension = os.path.splitext(original_file_name)
     new_file_name = f"{file_base}_{today_date}{file_extension}"
+    
     print(f" - today's date: {today_date}")
     print(f" - original file name: {original_file_name}")
     print(f" - new file name: {new_file_name}")
+    
     return new_file_name
 
 def prepareElinkProductionData(excel_file, download_dir, data_dir):
     print("Preparing e-link production data...")
+
     tools.makeDir(data_dir)
     original_path   = download_dir + excel_file
     new_path        = data_dir + excel_file
     new_path        = addDateToFileName(new_path)
-    # try:
-    #     shutil.move(original_path, new_path)
-    # except FileNotFoundError:
-    #     print(f"ERROR: Data file '{original_path}' not found.")
-    #     sys.exit(1)
+    
     print(f"Moving file to '{new_path}'...")
     tools.moveFile(original_path, new_path)
+    
     print("Done!")
 
 def main():
     excel_file      = "Harness_Serial_Number.xlsx"
     download_dir    = "/Users/caleb/Downloads"
     data_dir        = "data"
+
     download_dir    = tools.appendSlash(download_dir)
     data_dir        = tools.appendSlash(data_dir)
 
