ebcf5225c523e502ecced58e55a35a66a52818c6
Caleb James Smith <colorado.caleb.smith@gmail.com>
2025-08-27 18:03:47 -0500

    Create versions of scripts for macOS: update bash, python, and script paths.

---
+++ scripts/test_colors_mac.sh
@@ -0,0 +1,5 @@
+#!/opt/homebrew/bin/bash
+# test_colors.sh
+
+python3.10 ~/CMS/Tracker/e-links/eLink_Instrumentation/scripts/test_colors.py
+
---
+++ scripts/wire_length_mac.sh
@@ -0,0 +1,5 @@
+#!/opt/homebrew/bin/bash
+# wire_length.sh
+
+python3.10 ~/CMS/Tracker/e-links/eLink_Instrumentation/scripts/wire_length.py
+
